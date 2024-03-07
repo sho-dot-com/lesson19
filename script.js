96053bfda3c2e43b7613421dac9a59431b6de99f
sho-dot-com <154467784+sho-dot-com@users.noreply.github.com>
2024-03-07 18:36:33 +0400

    lesson 19

---
+++ script.js
@@ -0,0 +1,43 @@
+
+// const hamburger = gsap.timeline({ paused: true });
+
+// gsap.set(["#top-line", "#bottom-line"], {
+// 	transformOrigin: "center"
+// });
+
+// hamburger
+// 	.to("#top-line", {
+// 		y: 10,
+// 		ease: "back.in(1.4)"
+// 	})
+// 	.to(
+// 		"#bottom-line",
+// 		{
+// 			y: -10,
+// 			ease: "back.in(1.4)"
+// 		},
+// 		0
+// 	)
+// 	.set("#middle-line", {
+// 		opacity: 0
+// 	})
+// 	.to("#top-line", {
+// 		rotate: 45,
+// 		ease: "back.out(1.4)"
+// 	})
+// 	.to(
+// 		"#bottom-line",
+// 		{
+// 			rotate: -45,
+// 			ease: "back.out(1.4)"
+// 		},
+// 		"<"
+// 	);
+
+// let burger = document.querySelector("#hamburger");
+
+// burger.addEventListener("click", (e) => {
+// 	hamburger.play();
+// });
+
+       
